3c50f435916e80bc5d47d3187b4f37654c8a171d
Chris Abrams <mail@chrisabrams.com>
2015-02-04 12:35:12 -0500

    files

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = (options) => {
+
+  var gulp       = options.gulp,
+      livereload = options.livereload
+
+  gulp.task('watch-handlebars', function() {
+
+    return gulp.src('**/*.hbs', {read: false})
+      .pipe(livereload())
+  })
+
+}
